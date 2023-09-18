75b0303955f2bf3d181f8333ad34ef1c98cf1486
윤상기 <sesiro01@gmail.com>
2023-09-18 14:44:34 +0900

    ..

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const debug = process.env.NODE_ENV !== "production";
-const repository = "https://timesky82.github.io/timesky-react/";
+// const repository = "https://timesky82.github.io/timesky-react/";
 const nextConfig = {
     reactStrictMode: true,
     basePath: "/{timesky82.github.io/timesky-react/}",
