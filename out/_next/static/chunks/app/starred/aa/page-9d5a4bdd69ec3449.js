a28623a96d987bb1abc88dfc03e615ba0acc9c7a
윤상기 <sesiro01@gmail.com>
2023-09-18 12:37:03 +0900

    ..

--- out/_next/static/chunks/app/starred/aa/page-9d5a4bdd69ec3449.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[905],{4154:function(e,n,r){Promise.resolve().then(r.bind(r,7837))},7837:function(e,n,r){"use strict";r.r(n),r.d(n,{default:function(){return s}});var t=r(7437);r(2265);var o=r(6970);function s(){let[e,n]=(0,o.x)({name:"Michel",age:33});return(0,t.jsxs)("div",{className:"App",children:[(0,t.jsxs)("h1",{children:["Hello ",e.name," (",e.age,")"]}),(0,t.jsx)("input",{onChange:e=>{var r;r=e.target.value,n(e=>{e.name=r})},value:e.name}),(0,t.jsx)("br",{}),(0,t.jsx)("button",{onClick:function(){n(e=>{e.age++})},children:"Older"})]})}},622:function(e,n,r){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var t=r(2265),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),u=Object.prototype.hasOwnProperty,a=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,i={key:!0,ref:!0,__self:!0,__source:!0};function c(e,n,r){var t,s={},c=null,l=null;for(t in void 0!==r&&(c=""+r),void 0!==n.key&&(c=""+n.key),void 0!==n.ref&&(l=n.ref),n)u.call(n,t)&&!i.hasOwnProperty(t)&&(s[t]=n[t]);if(e&&e.defaultProps)for(t in n=e.defaultProps)void 0===s[t]&&(s[t]=n[t]);return{$$typeof:o,type:e,key:c,ref:l,props:s,_owner:a.current}}n.Fragment=s,n.jsx=c,n.jsxs=c},7437:function(e,n,r){"use strict";e.exports=r(622)}},function(e){e.O(0,[970,971,596,744],function(){return e(e.s=4154)}),_N_E=e.O()}]);
