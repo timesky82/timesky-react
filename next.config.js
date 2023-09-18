e12311f2042978056cd6e7f1f7945a32a081b995
윤상기 <sesiro01@gmail.com>
2023-09-18 10:50:06 +0900

    first

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const debug = process.env.NODE_ENV !== "production";
+const repository = "https://timesky82.github.io/timesky-react";
+const nextConfig = {
+    reactStrictMode: true,
+    assetPrefix: !debug ? `/${repository}/` : "",
+    trailingSlash: true,
+    output: 'export',
+};
+// export const prefix =
+//     process.env.NODE_ENV === "production"
+//         ? "https://timesky82.github.io/timesky-react"
+//         : "";
 
 module.exports = nextConfig
