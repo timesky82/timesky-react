d607b07bba12f79f220298ea272a30e09afc9eaf
윤상기 <sesiro01@gmail.com>
2023-09-25 17:16:19 +0900

    loading

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,16 +1,23 @@
 "use client";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Container from "@mui/material/Container";
+import { useLottie } from "lottie-react";
+import ani_loading from "@/../../public/lottie/ani_loading.json";
 
-interface User {
-  id: number;
-  name: string;
-}
-const prams = {
-  fsym: "BTC",
-};
 export default function TasksPage() {
+  const [loading, setLoading] = useState(true);
+  const [xrpData, setXrpData] = useState({
+    USD: 0,
+    JPY: 0,
+    KRW: 0,
+  });
+  const options = {
+    animationData: ani_loading,
+    loop: true,
+  };
+
+  const { View } = useLottie(options);
+
   useEffect(() => {
     console.log("Loading...");
     const fetchData = async (params: any) => {
@@ -24,8 +31,12 @@ export default function TasksPage() {
 
         {
           data.Response
-            ? console.log(data.Response ,':', data.Message)
-            : console.log(data);
+            ? console.log(data.Response, ":", data.Message)
+            : (console.log(data),
+              setTimeout(() => {
+                setLoading(false);
+              }, 0),
+              setXrpData(data));
         }
       } catch (error) {
         console.log(error);
@@ -33,12 +44,20 @@ export default function TasksPage() {
     };
 
     fetchData({
-      fsym: "XRPaaa",
+      fsym: "XRP",
       tsyms: "USD,JPY,KRW",
     });
   }, []);
 
-  return <div className="App">See console</div>;
-
-  return <Container></Container>;
+  return (
+    <div className="App">
+      {loading ? (
+        <div>{View}</div>
+      ) : (
+        <div>
+          {xrpData.KRW}원,{xrpData.USD}$,{xrpData.JPY}엔
+        </div>
+      )}
+    </div>
+  );
 }
