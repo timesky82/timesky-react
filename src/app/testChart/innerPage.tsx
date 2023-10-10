3b84ebf5a8d57dd1da571d9991386da1ca3ce82d
윤상기 <sesiro01@gmail.com>
2023-10-10 15:09:26 +0900

    ..

--- src/app/testChart/innerPage.tsx
+++ src/app/testChart/innerPage.tsx
@@ -2,21 +2,14 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useLottie } from "lottie-react";
-import ani_loading from "@/../../public/lottie/ani_loading.json";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
+import loading_draw from "@/../../public/lottie/loading_draw.json";
 import "@/../../public/scss/sample.scss";
-
+;
 export default function TasksPage() {
   const [loading, setLoading] = useState(true);
   const [coinData, setCoinData] = useState([] as any[]);
   const lottieOptions = {
-    animationData: ani_loading,
+    animationData: loading_draw,
     loop: true,
   };
 
@@ -54,70 +47,16 @@ export default function TasksPage() {
   }, []);
 
   return (
-    <div>
+    <>
       {loading ? (
         <div style={{ minWidth: "280px", width: "50vw", margin: "100px auto" }}>
           {View}
         </div>
       ) : (
-        <div>
-          <TableContainer
-            sx={{
-              maxHeight: "100vh",
-              margin: "0 auto",
-            }}
-          >
-            <Table stickyHeader aria-label="sticky table">
-              <TableHead>
-                <TableRow>
-                  <TableCell align={"center"}>
-                    <strong className="text_h3">인덱스</strong>
-                  </TableCell>
-                  <TableCell align={"center"}><strong className="text_h3">로고</strong></TableCell>
-                  <TableCell align={"right"}><strong className="text_h3">가격</strong></TableCell>
-                  <TableCell align={"center"}><strong className="text_h3">Full Name</strong></TableCell>
-                  <TableCell align={"center"}><strong className="text_h3">평가</strong></TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {coinData.map((coinData, i) => (
-                  <TableRow key={i}>
-                    <TableCell component="td" scope="row" align="center">
-                      {i}
-                    </TableCell>
-                    <TableCell align="center">
-                      <LazyLoadImage
-                        src={
-                          "https://www.cryptocompare.com/" +
-                          coinData.CoinInfo.ImageUrl
-                        }
-                        height={"30px"}
-                        width={"30px"}
-                        alt="lorempic"
-                        effect="blur"
-                        placeholderSrc={
-                          "https://basicit.co.kr/img/assets/favicon.png"
-                        }
-                      />
-                    </TableCell>
-                    <TableCell align="right" sx={{ whiteSpace: "nowrap" }}>
-                      {coinData.DISPLAY?.KRW.PRICE}
-                    </TableCell>
-
-                    <TableCell align="center">
-                      {coinData.CoinInfo.FullName}
-                    </TableCell>
-
-                    <TableCell align="center">
-                      {coinData.CoinInfo.Rating.Weiss.Rating}
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+        <div >
+          test
         </div>
       )}
-    </div>
+    </>
   );
 }
