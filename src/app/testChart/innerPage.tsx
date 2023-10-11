3f2377f3935f62734ca3e7361b63f3e36b06eff7
윤상기 <sesiro01@gmail.com>
2023-10-11 14:35:04 +0900

    stop

--- src/app/testChart/innerPage.tsx
+++ src/app/testChart/innerPage.tsx
@@ -4,10 +4,12 @@ import axios from "axios";
 import { useLottie } from "lottie-react";
 import loading_draw from "@/../../public/lottie/loading_draw.json";
 import "@/../../public/scss/sample.scss";
-;
+
+import SampleChart from "./ChartSample";
 export default function TasksPage() {
   const [loading, setLoading] = useState(true);
   const [coinData, setCoinData] = useState([] as any[]);
+  const [mapData, setmapData] = useState([] as any[]);
   const lottieOptions = {
     animationData: loading_draw,
     loop: true,
@@ -25,27 +27,35 @@ export default function TasksPage() {
             params: params,
           }
         );
-
         {
           data.Response
             ? console.log(data.Response, ":", data.Message)
             : (console.log(data.Data),
+              setCoinData(data.Data),
               setTimeout(() => {
                 setLoading(false);
-              }, 2000),
-              setCoinData(data.Data));
+              }, 2000));
         }
       } catch (error) {
         console.log(error);
       }
     };
-
     fetchData({
       limit: 50,
       tsym: "KRW",
     });
-  }, []);
 
+    const dataTreeMap = coinData.map((data) => {
+      return {
+        name: data.CoinInfo.FullName,
+        id: data.CoinInfo.Id,
+        price: data.RAW.KRW.PRICE,
+        cap: data.RAW.KRW.MKTCAP,
+        img: data.CoinInfo.ImageUrl,
+      };
+    });
+    setmapData(dataTreeMap);
+  }, [loading]);
   return (
     <>
       {loading ? (
@@ -53,8 +63,9 @@ export default function TasksPage() {
           {View}
         </div>
       ) : (
-        <div >
+        <div>
           test
+          <SampleChart chartID="tree-map" chartData={mapData}></SampleChart>
         </div>
       )}
     </>
