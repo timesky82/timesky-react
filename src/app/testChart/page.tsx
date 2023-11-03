4abf77c4cd524a4737773f3bea92505930cf0844
윤상기 <sesiro01@gmail.com>
2023-11-03 13:59:53 +0900

    ...

--- src/app/testChart/page.tsx
+++ src/app/testChart/page.tsx
@@ -1,12 +1,12 @@
-// import AxiosTestCoin from "./innerPage";
-// import type { Metadata } from "next";
-// export const metadata: Metadata = {
-//   title: "Axios Test Coin / 베이직",
-//   openGraph: {
-//     title: "Axios Test",
-//     description: "React , Mui, NextJs, cryptocompare ",
-//   },
-// };
-// export default function test01() {
-//   return <AxiosTestCoin />;
-// }
+import AxiosTestCoin from "./innerPage";
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Axios Test Coin / 베이직",
+  openGraph: {
+    title: "Axios Test",
+    description: "React , Mui, NextJs, cryptocompare ",
+  },
+};
+export default function test01() {
+  return <AxiosTestCoin />;
+}
